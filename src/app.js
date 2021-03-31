f5c4a1f570e93772439f95216f79ca53ac9f8b6e
yasuhei cristiano nakamura <yasuhei_nakamura@hotmail.com>
2021-03-31 18:32:49 -0300

    express API with Games Endpoints

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const connect = require('./models/index')
+const gamesRouter = require('./routes/games');
+const app = express();
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cookieParser());
+
+connect();
+
+app.get('/', (req, res) => {
+  return res.json({message: 'API ok'});
+});
+
+app.use('/games', gamesRouter);
+
+app.listen(3000, () => {
+  console.log('Api running on port 3000');
+});
---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const {connect} = require('mongoose');
+const MONGODB_URL = process.env.DATABASE;
+
+module.exports = () => {
+  const options = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify:false,
+    useUnifiedTopology: true,
+  };
+  return connect(MONGODB_URL, options);
+
+}
